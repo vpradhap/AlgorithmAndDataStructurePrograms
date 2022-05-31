83bd3d8f15beb8de45e4ad44ec295f6e2bec5c9b
vpradhap <pradhapvenkat@gmail.com>
2022-05-31 15:01:52 +0530

    Added : Anagram Detection program

---
+++ Anagrams.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAndDataStructurePrograms
+{
+    public class Anagrams
+    {
+        public static void Anagram()
+        {
+            Console.Write("\nEnter the String 1 : ");
+            string str1 = Console.ReadLine();
+            Console.Write("\nEnter the String 2 : ");
+            string str2 = Console.ReadLine();
+            char[] ch1 = str1.ToUpper().ToCharArray();
+            char[] ch2 = str2.ToUpper().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string newStr1 = new string(ch1);
+            string newStr2 = new string(ch2);
+            if (newStr1 == newStr2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nBoth the strings are anagrams");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nBoth the strings are not anagrams");
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("\n\t\tAlgorithm and Data structure programs");
+﻿using AlgorithmAndDataStructurePrograms;
+
+Console.WriteLine("\n\t\tAlgorithm and Data structure programs");
+
+int flag =0;
+while(flag == 0)
+{
+    Console.WriteLine("\n1 - Anagram program");
+    Console.Write("\nEnter your choice : ");
+    string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            Anagrams.Anagram();
+            break;
+        default :
+            flag = 1;
+            Console.WriteLine("\nEnter valid number");
+            break;
+    }
+}
