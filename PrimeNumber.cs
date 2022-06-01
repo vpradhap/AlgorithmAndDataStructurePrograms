2d45b0d47526268bbf6af0a9f8dff8619df148d9
vpradhap <pradhapvenkat@gmail.com>
2022-06-01 13:01:37 +0530

    Added : Prime numbers from 1 to 1000 program

---
+++ PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAndDataStructurePrograms
+{
+    public class PrimeNumber
+    {
+        public static void Prime()
+        {
+            int count = 0;
+            Console.Write("\nPrime numbers between 1 to 1000 \n\n");
+            //Logic
+            for (int num = 2;num < 1000;num++)
+            {
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 0)
+                {
+                    Console.Write(num + "  ");
+                }
+                count = 0;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ int flag =0;
 while(flag == 0)
 {
     Console.WriteLine("\n1 - Anagram program");
+    Console.WriteLine("2 - Prime numbers between 1 to 1000 program");
     Console.Write("\nEnter your choice : ");
     string choice = Console.ReadLine();
     switch (choice)
     {
         case "1":
             Anagrams.Anagram();
+            break;
+        case "2":
+            PrimeNumber.Prime();
+            Console.WriteLine();
             break;
         default :
             flag = 1;
